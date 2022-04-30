506b68dfed1df40b9d6454050dee371c9c85edab
Felix <741883227@qq.com>
2022-04-30 10:51:18 +0800

    template

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 //
-import App from './App';
+import App from './App.tsx';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
